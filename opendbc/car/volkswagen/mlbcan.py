434cae465149e25385c5fd0c4b1b9f94da77841d
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-09-11 01:02:58 +0200

    TEST

--- opendbc/car/volkswagen/mlbcan.py
+++ opendbc/car/volkswagen/mlbcan.py
@@ -67,8 +67,8 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
     "ACC_neg_Sollbeschl_Grad": 4.0 if acc_enabled else 0,
     "ACC_pos_Sollbeschl_Grad": 4.0 if acc_enabled else 0,
     "ACC_Dynamik": 2,
-    "ACC_Anfahren": starting,
-    "ACC_Anhalten": stopping,
+    "ACC_Anfahren": 0,
+    "ACC_Anhalten": 1,
   }
   commands.append(packer.make_can_msg("ACC_01", bus, acc_01_values))
 
